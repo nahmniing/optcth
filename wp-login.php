285206b728e525a86a9d1a84a6167831f773e6cd
nahmniing <necromonkerzero@gmail.com>
2016-11-15 15:48:41 +0700

    Initial Site

---
+++ wp-login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="th">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="th">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>OPTC-TH &lsaquo; เข้าสู่ระบบ</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://nahmniing.github.io/optcth/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6.1' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-th">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="สร้างขึ้นโดยใช้เวิร์ดเพรส" tabindex="-1">OPTC-TH</a></h1>
+	
+<form name="loginform" id="loginform" action="https://nahmniing.github.io/optcth/wp-login.php" method="post">
+	<p>
+		<label for="user_login">ชื่อผู้ใช้ หรือ อีเมล<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">รหัสผ่าน<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> บันทึกการใช้งานของฉัน</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="เข้าสู่ระบบ" />
+		<input type="hidden" name="redirect_to" value="https://nahmniing.github.io/optcth/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://nahmniing.github.io/optcth/wp-login.php?action=lostpassword">คุณจำรหัสผ่านไม่ได้?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://nahmniing.github.io/optcth/">&larr; กลับไปที่เว็บ OPTC-TH</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
